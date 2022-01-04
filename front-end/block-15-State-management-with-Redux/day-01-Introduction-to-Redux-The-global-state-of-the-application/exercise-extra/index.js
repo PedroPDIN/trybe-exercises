6ce01c497da12cea7ab7927575fe4cbb0e64492e
Pedro <pedro.oficial1969@gmail.com>
2022-01-03 22:05:52 -0300

    docs: adiciona exercicio extra da plataforna "codesandbox".

---
+++ front-end/block-15-State-management-with-Redux/day-01-Introduction-to-Redux-The-global-state-of-the-application/exercise-extra/index.js
@@ -0,0 +1,54 @@
+const showCountValue = (counter) => {
+     const h1Element = document.getElementById("counter");
+     h1Element.innerText = counter;
+   };
+   
+   showCountValue(0);
+   // ========== ACTIONTYPES ==========
+   const INCREMENTO = "INCREMENTO";
+   const DECREMENTO = "DECREMENTO";
+   
+   // ========== ACTIONS ==========
+   const actionIncremento = (number) => {
+     // DISPARA A ACTION DE ADICIONAR + 1 AO COUNTER
+     //store.???
+     return {
+       type: INCREMENTO,
+       number,
+     }
+   };
+   
+   const actionDecremento = (number) => {
+     // DISPARA A ACTION DE REMOVER - 1 AO COUNTER
+     //store.???
+    return {
+      type: DECREMENTO,
+      number,
+    }
+   };
+   // ========== REDUCER ==========
+   const initialState = { value: 0};
+   const reducer = (state = initialState, action) => {
+     switch (action.type) {
+       case INCREMENTO:
+         return { value: state.value + action.number };
+         case DECREMENTO:
+         return { value: state.value - action.number };
+       default:
+         return state;
+     }
+   };
+   
+   // ========== STORE ==========
+   const store = Redux.createStore(reducer);
+   
+   store.subscribe(() => {
+     const counter = store.getState();
+     showCountValue(counter.value);
+   });
+   
+   const btnInc = document.getElementById("inc");
+   const btnDec = document.getElementById("dec");
+   
+   btnInc.addEventListener("click", () => store.dispatch(actionIncremento(10)));
+   btnDec.addEventListener("click", () => store.dispatch(actionDecremento(5)));
