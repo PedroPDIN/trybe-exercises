deaa9c2795259261f279f88bd1b899b8fddab9a8
Pedro <pedro.oficial1969@gmail.com>
2022-01-07 20:54:34 -0300

    adiciona inicio para fazer o exercicio de fixação

---
+++ front-end/block-15-State-management-with-Redux/day-05-Tests-in-React-Redux/exercise-to-fix/my-test/src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { renderWithRedux } from "./renderWithRedux";
+import App from './App';
+
+describe('testing clicks', () => {
+     beforeEach(cleanup);
+     test('the page should has a button and a text 0', () => {
+       const { queryByText } = renderWithRedux(<App />);
+       const buttonAdicionar = queryByText('Clique aqui');
+   
+       expect(buttonAdicionar).toBeInTheDocument();
+       expect(queryByText('0')).toBeInTheDocument();
+     });
+   });
