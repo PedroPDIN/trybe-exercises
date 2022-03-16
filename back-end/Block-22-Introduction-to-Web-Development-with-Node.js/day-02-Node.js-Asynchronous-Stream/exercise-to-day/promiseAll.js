250c8df5ce7f299f6502d918d123da2d6c0a1d62
Pedro Lima <pedro.oficial1969@gmail.com>
2022-03-15 21:12:56 -0300

    feat(exercise-to-day): adiciona exercicio 5 completo.

--- back-end/Block-22-Introduction-to-Web-Development-with-Node.js/day-02-Node.js-Asynchronous-Stream/exercise-to-day/promiseAll.js
+++ back-end/Block-22-Introduction-to-Web-Development-with-Node.js/day-02-Node.js-Asynchronous-Stream/exercise-to-day/promiseAll.js
@@ -1,18 +1,29 @@
 const fs = require("fs").promises;
 
-const files = () => {
+async function main() { // usar async/await foi inspirada no gabarito.
   const arrayValues = ["Finalmente", "estou", "usando", "Promise.all", "!!!"];
 
-  arrayValues.map((string, index) => fs.writeFile(`./file${index + 1}.txt`, string));
+  const files = arrayValues.map((string, index) => fs.writeFile(`./file${index + 1}.txt`, string));
 
-  Promise.all([
-    fs.readFile('./file1.txt'),
-    fs.readFile('./file2.txt'),
-    fs.readFile('./file3.txt'),
-    fs.readFile('./file4.txt'),
-    fs.readFile('./file5.txt')
-  ])
-  .then((datas) => fs.readFile(datas, ))
+  await Promise.all(files)
+
+   const fileNames = [
+     './file1.txt',
+     './file2.txt',
+     './file3.txt',
+     './file4.txt',
+     './file5.txt',
+   ]; // solução inspirado no gabarito.
+      // no caso criar um array dedicado aos arquivos pra logo depois fazer a leitura.
+
+  const contents = await Promise.all(
+    fileNames.map((content) => fs.readFile(content, 'utf-8'))
+  )
+
+  const newContent = contents.join(' ');
+  // irá retornar um array com os valores de cada arquivo. Com isso, o uso do join() para "juntar" os valores.
+
+  await fs.writeFile('./fileAll.txt', newContent)
 };
 
-console.log(files())
+main()
