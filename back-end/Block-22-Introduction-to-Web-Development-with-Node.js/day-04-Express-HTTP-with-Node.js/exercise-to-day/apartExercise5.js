b2fb0aa6cbb0333093bd6161dbf886ee6539d9f1
Pedro Lima <pedro.oficial1969@gmail.com>
2022-03-19 18:10:28 -0300

    feat(exercise-5): adiciona exercício 5 completo.

---
+++ back-end/Block-22-Introduction-to-Web-Development-with-Node.js/day-04-Express-HTTP-with-Node.js/exercise-to-day/apartExercise5.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fs = require("fs").promises;
+// const bodyParser = require("body-parser")
+
+const app = express();
+
+const fileJson = () => {
+  return fs.readFile("./simpsons.json", "utf-8")
+  .then((data) => JSON.parse(data))
+};
+
+app.get("/", async (req, res) => {
+  const dataSimpsons = await fileJson()
+  try {
+    res.status(200).json(dataSimpsons);
+  } catch (error) {
+    res.status(500).json({ "message": "Internal Server Error" })
+  }
+})
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na posta 3001 (simpsons)')
+});
--- back-end/Block-22-Introduction-to-Web-Development-with-Node.js/day-04-Express-HTTP-with-Node.js/exercise-to-day/index.js
+++ back-end/Block-22-Introduction-to-Web-Development-with-Node.js/day-04-Express-HTTP-with-Node.js/exercise-to-day/index.js
@@ -4,17 +4,31 @@ const bodyParser = require("body-parser")
 const app = express();
 app.use(bodyParser.json())
 
+// EXERCÍCIO 1
 app.get("/ping", (req, res) => {
   return res.status(200).json({ message: 'pong'})
 });
 
-/* app.post("/hello", (req, res) => {
+// EXERCÍCIO 2
+app.post("/hello", (req, res) => {
   const { name } = req.body;
   return res.status(200).json({ message: `Hello, ${name}!` })
-}) */
+})
 
-/* app.post("/greetings", (req, res) => {
+// EXERCÍCIO 3
+app.post("/greetings", (req, res) => {
   const { name, age } = req.body;
-}) */
+  const AVG_AGE = 17;
+
+  if(age > AVG_AGE) return res.status(200).json({ "message": `Hello, ${name}`});
+  else return res.status(401).json({ "message": "Unauthorized" })
+})
+
+// EXERCÍCIO 4
+app.put("/users/:name/:age", (req, res) => {
+  const { name, age } = req.params
+  res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade`})
+})
+
 
 app.listen(3000, () => console.log("Rodando da aplicação da posta 3000"));
