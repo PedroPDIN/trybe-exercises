70c1c8c156117394312d987b3f188412ee5dccdb
Pedro Lima <pedro.oficial1969@gmail.com>
2022-05-10 14:11:05 -0300

    model com SQL

--- back-end/Block-26-TypeScript/day-02-Static-Typing-and-Generics/exercise-to-practice/dist/src/practice3.dog.js
+++ back-end/Block-26-TypeScript/day-02-Static-Typing-and-Generics/exercise-to-practice/dist/src/practice3.dog.js
@@ -8,5 +8,5 @@ class Dog {
         return `A raça do ${this.nome} é ${this.raca};`;
     }
 }
-const dog1 = new Dog(2, 'pitbull');
+const dog1 = new Dog('bartolomeu', 'pitbull');
 console.log(dog1.speak());
