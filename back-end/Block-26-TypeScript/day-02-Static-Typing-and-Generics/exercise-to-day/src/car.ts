2ee999da7d52138d025375e34402b2a3cabdd050
Pedro Lima <pedro.oficial1969@gmail.com>
2022-05-12 10:38:36 -0300

    feat(exercise-01): adiciona exercicio 01 completo, no dia 02.

--- back-end/Block-26-TypeScript/day-02-Static-Typing-and-Generics/exercise-to-day/src/car.ts
+++ back-end/Block-26-TypeScript/day-02-Static-Typing-and-Generics/exercise-to-day/src/car.ts
@@ -1,15 +1,23 @@
-enum colors {
+export enum colors {
   BLACK = "Preto",
   WHITE = "Branco",
   RED = "Vermelho",
   SILVER = "Prata",
 }
 
-enum doors {
-
+export enum doors {
+  DOOR1 = 'Porta do motorista',
+  DOOR2 = 'Porta do carona',
+  DOOR3 = 'Porta do passageiro a esquerda',
+  DOOR4 = 'Porta do passageiro a direita',
 }
 
-class car {
+export enum directions {
+  RIGHT = 'Direção para direita.',
+  LEFT = 'Direção a esquerda',
+}
+
+export class Car {
 
   brand: string;
   color: colors;
@@ -25,7 +33,35 @@ class car {
     console.log('BI BI BI!!!')
   };
 
-  openTheDoor(): void {
-    console.log('')
+  openTheDoor(door: doors): void {
+    console.log(`Abrir ${ door }`)
+  };
+
+  closeTheDoor(door: doors): void {
+    console.log(`Fecha ${ door }`)
+  };
+
+  turnOn(): void {
+    console.log(`Lingando o carro.`)
+  };
+
+  turnOff(): void {
+    console.log('Desligando o carro.')
+  };
+
+  speedUP(): void {
+    console.log('Acelerando o carro.')
+  };
+
+  speedDown(): void {
+    console.log('Reduzindo a velocidade do carro.')
+  };
+
+  stop(): void {
+    console.log('Parando o carro.')
+  };
+  
+  turn(direction: directions): void {
+    console.log(`Movendo a ${direction}`)
   }
 }
