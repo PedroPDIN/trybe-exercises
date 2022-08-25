4b9adef3d2b18a64171acfa56c2f4f18fd1fc6d1
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-25 13:32:12 -0300

    fix: alteração minimalista

--- computer-science/Block-36-Complexity-of-Algorithms/day-01-Complexity-of-Algorithms/example/example_logaritimica.py
+++ computer-science/Block-36-Complexity-of-Algorithms/day-01-Complexity-of-Algorithms/example/example_logaritimica.py
@@ -6,7 +6,6 @@ def binary_search(numbers, target):
 
     while start <= end: # os índices podem ser no máximo iguais, o início não pode ultrapassar o fim
         mid = (start + end) // 2 # encontro o meio
-        print(mid)
 
 
         if numbers[mid] == target: # se o elemento do meio for o alvo, devolve a posição do meio
